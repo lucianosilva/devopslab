7247066f06c80e79c8475182d5d021843d475659
Luciano Silva Carmo <lcarmo2701@gmail.com>
2022-06-22 22:20:00 -0300

    GCP Deploy fixes

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps V5"
+    return "Laboratório Pipeline DevOps V6 - Rodando no GCP"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
